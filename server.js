a4e2f8fe3d16638db69a799cc8417a42205753a5
Zsolt Meszaros <zsolt@sowad.com>
2017-03-06 07:09:05 +0100

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
+app.listen(port, function() {
+  console.log('Express server is up on port', port);
 });
